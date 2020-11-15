3e41c767786db06a8b9b4b3307c908ac89d092a7
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-11-15 12:24:59 +0800

    [os] paging

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,39 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use x86_64;
 mod gdt;
 mod interrupts;
+mod memory;
 mod serial;
 mod vga_buffer;
 
 #[cfg(not(test))]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("starting...");
     init();
     println!("init [ok]");
 
-    // occur page fault
-    let ptr = 0x2031b2 as *mut u32;
-    unsafe {
-        let x = *ptr;
-    }
-    println!("read worked");
-    unsafe {
-        *ptr = 42;
-    }
-    println!("write worked");
+    use memory::BootInfoFrameAllocator;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     hlt_loop();
 }
@@ -56,8 +65,7 @@ fn panic(info: &PanicInfo) -> ! {
 
 // Tests
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init(); // new
     test_main();
     hlt_loop();
